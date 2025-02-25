a5a2431156e5e20efd96d8b17e8fab3e55a689c9
asare-develops <139370967+asare-develops@users.noreply.github.com>
2025-02-25 15:32:58 +0000

    Updated JS code

---
+++ Desktop/Full Stack JS/01. Basics/app.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+console.warn("this is a warning");
+console.table({ name: "HARRISON", age: 22, location: "Ablekuma" });
+
+const name = "Harrison";
+let whatDoYouWannaBecome = "Programmer";
+const gender = "Male";
+const twitterHandle = "asaredevelops";
+
+console.log(name);
+console.log(whatDoYouWannaBecome);
+console.log(gender);
+console.log(twitterHandle);
